b06706f8800d9a3d0f0155d2001afb60e2efbb2a
joaocmfnogueira <joao.nogueira@estudante.ufjf.br>
2023-07-06 16:05:17 -0300

    criação da tela de inicio

--- T3/index.js
+++ T3/index.js
@@ -12,7 +12,6 @@ import {
 import { atualizarObjetos, fadePlanos } from "./src/js/actions.js";
 import { keyboardUpdate, rotacaoMouse } from "./src/js/controls.js";
 import * as THREE from "three";
-import {GLTFLoader} from '../build/jsm/loaders/GLTFLoader.js'
 
 export const cameraHolder = criarCameraHolder();
 export const aviao = criarAviao();
@@ -28,52 +27,28 @@ export const torretas = [];
 
 // let pressed = false;
 
-let mixer, audioLoader, audioPath, r2d2, time = 0;
+let mixer, audioLoader, audioPath, time = 0;
 let clock = new THREE.Clock();
 
 const canvas = document.querySelector("canvas");
 canvas.style.cursor = "none";
 
-// const startButton = document.getElementById( 'startButton' );
-// 			startButton.addEventListener( 'click', function () {
 
-// 				Ammo().then( function () {
-
-// 					init();
-// 					animate();
-
-// 				} );
-
-// 			} );
-
-// const loadingManager = new THREE.LoadingManager( () => {
-//   let loadingScreen = document.getElementById( 'loading-screen' );
-//   console.log(loadingScreen);
-//   loadingScreen.transition = 0;
-//   loadingScreen.style.setProperty('--speed1', '0');  
-//   loadingScreen.style.setProperty('--speed2', '0');  
-//   loadingScreen.style.setProperty('--speed3', '0');      
-
-//   let button  = document.getElementById("myBtn")
-//   button.style.backgroundColor = 'Red';
-//   button.innerHTML = 'Click to Enter';
-//   button.addEventListener("click", onButtonPressed);
-// });
-
+// loadAudio('../assets/sounds/imperial.mp3');
+TelaInicio();
 windowResize();
 clickListener();
 
 construirCena();
 criarPlanosIniciais();
 
-// loadAudio(loadingManager, '../assets/sounds/imperial.mp3');
 
 render();
 
 function render() {
-  // if(pressed){s
-    // const delta = clock.getDelta();
-    // if ( mixer !== undefined ) mixer.update( delta );
+  // const delta = clock.getDelta();
+  //   if ( mixer !== undefined ) mixer.update( delta );
+
   atualizarObjetos();
   keyboardUpdate();
   rotacaoMouse();
@@ -87,28 +62,39 @@ function render() {
   
 }
 
+function TelaInicio(){
+  // let loadingScreen = document.getElementById( 'loading-screen' );
+  // // loadingScreen.transition = 0;
+  // // loadingScreen.style.setProperty('--speed1', '0');  
+  // // loadingScreen.style.setProperty('--speed2', '0');  
+  // // loadingScreen.style.setProperty('--speed3', '0');      
+  console.log("aaa");
+  let button  = document.getElementById("myBtn");
+  button.style.backgroundColor = 'Red';
+  button.innerHTML = 'Click to Enter';
+  button.addEventListener("click", onButtonPressed);
+}
 
-// function onButtonPressed() {
-//   const loadingScreen = document.getElementById( 'loading-screen' );
-//   loadingScreen.transition = 0;
-//   loadingScreen.classList.add( 'fade-out' );
-//   loadingScreen.addEventListener( 'transitionend', (e) => {
-//     const element = e.target;
-//     element.remove();  
-//   });  
-//   // Config and play the loaded audio
-//   let sound = new THREE.Audio( new THREE.AudioListener() );
-//   audioLoader.load( audioPath, function( buffer ) {
-//     sound.setBuffer( buffer );
-//     sound.setLoop( true );
-//     sound.play(); 
-//   });
-// }
+function onButtonPressed() {
+  const loadingScreen = document.getElementById( 'loading-screen' );
+  loadingScreen.transition = 0;
+  loadingScreen.classList.add( 'fade-out' );
+  loadingScreen.addEventListener( 'transitionend', (e) => {
+    const element = e.target;
+    element.remove();  
+  });  
+  // Config and play the loaded audio
+  // let sound = new THREE.Audio( new THREE.AudioListener() );
+  // audioLoader.load( audioPath, function( buffer ) {
+  //   sound.setBuffer( buffer );
+  //   sound.setLoop( true );
+  //   sound.play(); 
+  // });
+}
 
-
-// function loadAudio(manager, audio)
+// function loadAudio(audio)
 // {
 //   // Create ambient sound
-//   audioLoader = new THREE.AudioLoader(manager);
+//   // audioLoader = new THREE.AudioLoader(manager);
 //   audioPath = audio;
 // }
